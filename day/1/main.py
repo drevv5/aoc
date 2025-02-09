ef92e173702866d6137e77d74bf071ea5e4167c2
drevv5 <drevv5@icloud.com>
2025-02-09 23:13:51 +0100

    part2

--- day/1/main.py
+++ day/1/main.py
@@ -19,3 +19,14 @@ for i in range(0, max(len(A), len(B))):
 
 print(reduce(add, A))
 
+with open("input.txt", 'r') as f:
+    lines = list(map(lambda x: x.strip('\n').split(), f.readlines()))
+    A, B = list(), list()
+    for i in range(0, len(lines)):
+        A.append(int(lines[i][0]))
+        B.append(int(lines[i][1]))
+
+for i in range(0, max(len(A), len(B))):
+    A[i] *= B.count(A[i])
+
+print(reduce(add, A))
